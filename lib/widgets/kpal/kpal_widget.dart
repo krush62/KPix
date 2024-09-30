770c0ce572d191b4194a1d42712d23d314e91996
Andreas Kruschinski <andreas.kruschinski@alcon.com>
2024-09-30 15:01:27 +0200

    Updated build scripts

--- lib/widgets/kpal/kpal_widget.dart
+++ lib/widgets/kpal/kpal_widget.dart
@@ -30,7 +30,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-library kpal;
 
 import 'dart:math';
 
---
+++ release_tools/deploy_android_all.ps1
@@ -0,0 +1,56 @@
+$jarSigner = "C:\Program Files\Android\Android Studio\jbr\bin\jarsigner.exe"
+$keyStore = "upload-keystore.jks"
+$keyStorePW = "upload-keystore-pw.txt"
+$apkSourcePath = "..\build\app\outputs\flutter-apk\app-release.apk"
+$bundleSourcePath = "..\build\app\outputs\bundle\release\app-release.aab"
+$keyStoreAlias = "upload"
+$yamlFile = "..\pubspec.yaml"
+$versionLine = Select-String -Path $yamlFile -Pattern "version: (\d+\.\d+\.\d+)" | ForEach-Object { $_.Matches.Groups[1].Value }
+$versionCode = ($versionLine -replace '\.')
+$versionCode = [int]$versionCode
+$androidApkOutputPath = "AndroidAPK"
+$androidBundleOutputPath = "AndroidBundle"
+$gradleFilePath = "..\android\app\build.gradle"
+$namespace = Select-String -Path $gradleFilePath -Pattern 'namespace "([^"]+)"' | ForEach-Object { $_.Matches.Groups[1].Value }
+
+Write-Host "Extracted Version: $versionLine"
+Write-Host "Version Code: $versionCode"
+Write-Host "Extracted Namespace: $namespace"
+
+Write-Host "Updating Gradle File"
+$gradleContent = Get-Content $gradleFilePath
+$gradleContent = $gradleContent -replace "flutterVersionName = '.*'", "flutterVersionName = '$versionLine'"
+$gradleContent = $gradleContent -replace "flutterVersionCode = '.*'", "flutterVersionCode = '$versionCode'"
+Set-Content -Path $gradleFilePath -Value $gradleContent
+
+
+cd ..\android
+Write-Host "Building APK..."
+.\gradlew.bat assembleRelease | Out-Null
+Write-Host "Building Bundle..."
+.\gradlew.bat bundleRelease | Out-Null
+Write-Host "Signing Bundle..."
+$pw = (Get-Content $keyStorePW).Trim()
+&$jarSigner -keystore $keyStore -storepass $pw $bundleSourcePath $keyStoreAlias | Out-Null
+
+
+cd $PSScriptRoot
+
+
+Write-Host "Copying APK..."
+if (-Not (Test-Path -Path $androidApkOutputPath)) {		
+	New-Item -Path ".\" -Name $androidApkOutputPath -ItemType "directory" | Out-Null
+}
+$fullApkPath = "$androidApkOutputPath\$namespace-v$versionLine.apk"
+Copy-Item $apkSourcePath -Destination $fullApkPath
+
+
+Write-Host "Copying Bundle..."
+if (-Not (Test-Path -Path $androidBundleOutputPath)) {		
+	New-Item -Path ".\" -Name $androidBundleOutputPath -ItemType "directory" | Out-Null
+}
+$fullBundlePath = "$androidBundleOutputPath\$namespace-v$versionLine.aab"
+Copy-Item $bundleSourcePath -Destination $fullBundlePath
+
+C:\Windows\explorer.exe "/select,`"$fullApkPath"`"
+C:\Windows\explorer.exe "/select,`"$fullBundlePath"`"

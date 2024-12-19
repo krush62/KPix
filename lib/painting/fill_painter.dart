f3be2bf4d1fd15943ac47df2481ddaced8150a4a
Andreas Kruschinski <andikrusch@yahoo.de>
2024-12-19 09:45:33 +0100

    Adding shading layer capabilities to fill tool

--- lib/painting/fill_painter.dart
+++ lib/painting/fill_painter.dart
@@ -20,6 +20,7 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:kpix/layer_states/drawing_layer_state.dart';
 import 'package:kpix/layer_states/layer_state.dart';
+import 'package:kpix/layer_states/shading_layer_state.dart';
 import 'package:kpix/managers/preference_manager.dart';
 import 'package:kpix/painting/itool_painter.dart';
 import 'package:kpix/painting/kpix_painter.dart';
@@ -97,17 +98,35 @@ class FillPainter extends IToolPainter
 
   @override
   void drawExtras({required final DrawingParameters drawParams}) {
-    if (_shouldDraw && drawParams.currentDrawingLayer != null)
+    if (_shouldDraw && (drawParams.currentDrawingLayer != null || drawParams.currentShadingLayer != null))
     {
-      if (drawParams.currentDrawingLayer!.visibilityState.value == LayerVisibilityState.visible && (drawParams.currentDrawingLayer!.lockState.value == LayerLockState.unlocked || (drawParams.currentDrawingLayer!.lockState.value == LayerLockState.transparency && drawParams.currentDrawingLayer!.getDataEntry(coord: _cursorPosNorm) != null)))
+      if ((drawParams.currentDrawingLayer != null && drawParams.currentDrawingLayer!.visibilityState.value == LayerVisibilityState.visible && (drawParams.currentDrawingLayer!.lockState.value == LayerLockState.unlocked || (drawParams.currentDrawingLayer!.lockState.value == LayerLockState.transparency && drawParams.currentDrawingLayer!.getDataEntry(coord: _cursorPosNorm) != null))) ||
+        (drawParams.currentShadingLayer != null && drawParams.currentShadingLayer!.visibilityState.value == LayerVisibilityState.visible && drawParams.currentShadingLayer!.lockState.value == LayerLockState.unlocked))
       {
         if (_options.fillAdjacent.value)
         {
-          _floodFill(fillColor: appState.selectedColor!, layer: drawParams.currentDrawingLayer!, start: _cursorPosNorm, doShade: shaderOptions.isEnabled.value, shadeDirection: shaderOptions.shaderDirection.value, shadeCurrentRampOnly: shaderOptions.onlyCurrentRampEnabled.value, fillWholeRamp: _options.fillWholeRamp.value);
+          if (drawParams.currentDrawingLayer != null)
+          {
+            _floodFill(fillColor: appState.selectedColor!, layer: drawParams.currentDrawingLayer!, start: _cursorPosNorm, doShade: shaderOptions.isEnabled.value, shadeDirection: shaderOptions.shaderDirection.value, shadeCurrentRampOnly: shaderOptions.onlyCurrentRampEnabled.value, fillWholeRamp: _options.fillWholeRamp.value);
+          }
+          else //SHADING LAYER
+          {
+            _floodFillShading(layer: drawParams.currentShadingLayer!, start: _cursorPosNorm, shadeDirection: shaderOptions.shaderDirection.value);
+            appState.rasterDrawingLayersBelow(layer: drawParams.currentShadingLayer!);
+          }
         }
         else
         {
-          _wholeFill(fillColor: appState.selectedColor!, layer: drawParams.currentDrawingLayer!, start: _cursorPosNorm, doShade: shaderOptions.isEnabled.value, shadeDirection: shaderOptions.shaderDirection.value, shadeCurrentRampOnly: shaderOptions.onlyCurrentRampEnabled.value, fillWholeRamp: _options.fillWholeRamp.value);
+          if (drawParams.currentDrawingLayer != null)
+          {
+            _wholeFill(fillColor: appState.selectedColor!, layer: drawParams.currentDrawingLayer!, start: _cursorPosNorm, doShade: shaderOptions.isEnabled.value, shadeDirection: shaderOptions.shaderDirection.value, shadeCurrentRampOnly: shaderOptions.onlyCurrentRampEnabled.value, fillWholeRamp: _options.fillWholeRamp.value);
+          }
+          else //SHADING LAYER
+          {
+            _wholeFillShading(layer: drawParams.currentShadingLayer!, start: _cursorPosNorm, shadeDirection: shaderOptions.shaderDirection.value);
+            appState.rasterDrawingLayersBelow(layer: drawParams.currentShadingLayer!);
+          }
+
         }
         hasHistoryData = true;
       }
@@ -216,6 +235,80 @@ class FillPainter extends IToolPainter
 
   }
 
+  void _floodFillShading({
+    required final ShadingLayerState layer,
+    required final CoordinateSetI start,
+    required final ShaderDirection shadeDirection,})
+  {
+    final int numRows = appState.canvasSize.y;
+    final int numCols = appState.canvasSize.x;
+    final List<List<bool>> visited = List<List<bool>>.generate(numCols, (final _) => List<bool>.filled(numRows, false));
+    final int? startValue = layer.getValueAt(coord: start);
+    final StackCol<CoordinateSetI> stackPoints = StackCol<CoordinateSetI>();
+    final HashMap<CoordinateSetI, int> addPixels = HashMap<CoordinateSetI, int>();
+    final Set<CoordinateSetI> removePixels = <CoordinateSetI>{};
+
+    stackPoints.push(CoordinateSetI(x: start.x, y: start.y));
+
+    while(stackPoints.isNotEmpty)
+    {
+      final CoordinateSetI curCoord = stackPoints.pop();
+      final int? shadeAtPos = layer.getValueAt(coord: curCoord);
+      if (!visited[curCoord.x][curCoord.y] && shadeAtPos == startValue)
+      {
+        visited[curCoord.x][curCoord.y] = true;
+        int shadeVal = shadeAtPos?? 0;
+
+        if (shadeDirection == ShaderDirection.right)
+        {
+           shadeVal++;
+        }
+        else
+        {
+          shadeVal--;
+        }
+
+        final int finalShadeVal = shadeVal.clamp(-ShadingLayerState.shadingMax, ShadingLayerState.shadingMax);
+        if (finalShadeVal == 0)
+        {
+           removePixels.add(curCoord);
+        }
+        else
+        {
+           addPixels[curCoord] = finalShadeVal;
+        }
+
+
+        if (curCoord.x + 1 < numCols)
+        {
+          stackPoints.push(CoordinateSetI(x: curCoord.x + 1, y: curCoord.y));
+        }
+        if (curCoord.x > 0)
+        {
+          stackPoints.push(CoordinateSetI(x: curCoord.x - 1, y: curCoord.y));
+        }
+        if (curCoord.y + 1 < numRows)
+        {
+          stackPoints.push(CoordinateSetI(x: curCoord.x, y: curCoord.y + 1));
+        }
+        if (curCoord.y > 0)
+        {
+          stackPoints.push(CoordinateSetI(x: curCoord.x, y: curCoord.y - 1));
+        }
+      }
+    }
+
+    if (removePixels.isNotEmpty)
+    {
+      layer.removeCoords(coords: removePixels);
+    }
+
+    if (addPixels.isNotEmpty)
+    {
+      layer.addCoords(coords: addPixels);
+    }
+  }
+
   void _wholeFill({
     required final ColorReference fillColor,
     required final DrawingLayerState layer,
@@ -291,6 +384,56 @@ class FillPainter extends IToolPainter
           }
         }
       }
+    }
+  }
+
+  void _wholeFillShading({
+    required final ShadingLayerState layer,
+    required final CoordinateSetI start,
+    required final ShaderDirection shadeDirection,
+  })
+  {
+    final int? startValue = layer.getValueAt(coord: start);
+    final HashMap<CoordinateSetI, int> addPixels = HashMap<CoordinateSetI, int>();
+    final Set<CoordinateSetI> removePixels = <CoordinateSetI>{};
+    for (int x = 0; x < appState.canvasSize.x; x++)
+    {
+      for (int y = 0; y < appState.canvasSize.y; y++)
+      {
+        final CoordinateSetI curCoord = CoordinateSetI(x: x, y: y);
+        final int? shadeAtPos = layer.getValueAt(coord: curCoord);
+        if (shadeAtPos == startValue)
+        {
+          int shadeVal = shadeAtPos?? 0;
+          if (shadeDirection == ShaderDirection.right)
+          {
+             shadeVal++;
+          }
+          else
+          {
+            shadeVal--;
+          }
+
+          final int finalShadeVal = shadeVal.clamp(-ShadingLayerState.shadingMax, ShadingLayerState.shadingMax);
+          if (finalShadeVal == 0)
+          {
+            removePixels.add(curCoord);
+          }
+          else
+          {
+            addPixels[curCoord] = finalShadeVal;
+          }
+        }
+      }
+    }
+    if (removePixels.isNotEmpty)
+    {
+      layer.removeCoords(coords: removePixels);
+    }
+
+    if (addPixels.isNotEmpty)
+    {
+      layer.addCoords(coords: addPixels);
     }
   }
 

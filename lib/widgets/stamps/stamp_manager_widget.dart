69bc45d6c8957511b6ecfed7a12fd2d6c7ab8de7
Andreas Kruschinski <andreas.kruschinski@alcon.com>
2025-01-17 16:07:07 +0100

    Prepared Widget for stamp manager

---
+++ lib/widgets/stamps/stamp_manager_widget.dart
@@ -0,0 +1,234 @@
+/*
+ * KPix
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get_it/get_it.dart';
+import 'package:kpix/managers/preference_manager.dart';
+import 'package:kpix/models/app_state.dart';
+import 'package:kpix/widgets/controls/kpix_animation_widget.dart';
+import 'package:kpix/widgets/overlays/overlay_entries.dart';
+import 'package:kpix/widgets/stamps/stamp_manager_entry_widget.dart';
+
+
+
+
+class StampManagerOptions
+{
+  final int colCount;
+  final double entryAspectRatio;
+  final double maxWidth;
+  final double maxHeight;
+  StampManagerOptions({required this.colCount, required this.entryAspectRatio, required this.maxWidth, required this.maxHeight});
+}
+
+class StampManagerWidget extends StatefulWidget
+{
+  final Function() dismiss;
+  final Function() fileLoad;
+  const StampManagerWidget({super.key, required this.dismiss, required this.fileLoad});
+
+  @override
+  State<StampManagerWidget> createState() => _StampManagerWidgetState();
+}
+
+class _StampManagerWidgetState extends State<StampManagerWidget>
+{
+  final OverlayEntryAlertDialogOptions _alertOptions = GetIt.I.get<PreferenceManager>().alertDialogOptions;
+  final StampManagerOptions _options = GetIt.I.get<PreferenceManager>().stampManagerOptions;
+  final ValueNotifier<List<StampManagerEntryWidget>> _fileEntries = ValueNotifier<List<StampManagerEntryWidget>>(<StampManagerEntryWidget>[]);
+  final ValueNotifier<StampManagerEntryWidget?> _selectedWidget = ValueNotifier<StampManagerEntryWidget?>(null);
+  late KPixOverlay _deleteWarningDialog;
+
+  @override
+  void initState()
+  {
+    super.initState();
+    _deleteWarningDialog = getTwoButtonDialog(
+      message: "Do you really want to delete this stamp?",
+      onNo: _deleteWarningNo,
+      onYes: _deleteWarningYes,
+      outsideCancelable: false,
+    );
+
+    _createWidgetList().then((final List<StampManagerEntryWidget> pList) {
+      _fileEntries.value = pList;
+    });
+  }
+
+  Future<List<StampManagerEntryWidget>> _createWidgetList() async
+  {
+    final List<StampManagerEntryWidget> fList = <StampManagerEntryWidget>[];
+    if (!kIsWeb)
+    {
+      final List<StampManagerEntryData> internalFiles = await loadStampsFromInternal();
+      for (final StampManagerEntryData fileData in internalFiles)
+      {
+        fList.add(StampManagerEntryWidget(selectedWidget: _selectedWidget, entryData: fileData));
+      }
+    }
+
+    return fList;
+  }
+
+  void _deleteWarningYes()
+  {
+    if (_selectedWidget.value != null)
+    {
+      deleteStamp(fullStampPath: _selectedWidget.value!.entryData.path).then((final bool success) {
+        _fileDeleted(success: success);
+      });
+    }
+    _deleteWarningDialog.hide();
+  }
+
+  void _fileDeleted({required final bool success})
+  {
+    if (success)
+    {
+      _createWidgetList().then((final List<StampManagerEntryWidget> fList) {
+        _selectedWidget.value = null;
+        _fileEntries.value = fList;
+      });
+    }
+  }
+
+  void _deleteWarningNo()
+  {
+    _deleteWarningDialog.hide();
+  }
+
+  void _dismissPressed()
+  {
+    widget.dismiss();
+  }
+
+  void _deleteStampPressed()
+  {
+    _deleteWarningDialog.show(context: context);
+  }
+
+  void _loadStamp()
+  {
+    //TODO
+  }
+
+  @override
+  Widget build(final BuildContext context)
+  {
+    return KPixAnimationWidget(
+      constraints: BoxConstraints(
+        minHeight: _alertOptions.minHeight,
+        minWidth: _alertOptions.minWidth,
+        maxHeight: _options.maxHeight,
+        maxWidth: _options.maxWidth,
+      ),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: _alertOptions.padding),
+          Text("STAMP MANAGER", style: Theme.of(context).textTheme.titleLarge),
+          SizedBox(height: _alertOptions.padding),
+          Expanded(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColorDark,
+                borderRadius: BorderRadius.all(Radius.circular(_alertOptions.borderRadius)),
+              ),
+              child: ValueListenableBuilder<List<StampManagerEntryWidget>>(
+                valueListenable: _fileEntries,
+                builder: (final BuildContext context, final List<StampManagerEntryWidget> sList, final Widget? child) {
+                  return GridView.extent(
+                    maxCrossAxisExtent: _options.maxWidth / _options.colCount,
+                    padding: EdgeInsets.all(_alertOptions.padding),
+                    childAspectRatio: _options.entryAspectRatio,
+                    mainAxisSpacing: _alertOptions.padding,
+                    crossAxisSpacing: _alertOptions.padding,
+                    children: sList.toList(),
+                  );
+                },
+              ),
+            ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Expanded(
+                child: Tooltip(
+                  message: "Close",
+                  waitDuration: AppState.toolTipDuration,
+                  child: Padding(
+                    padding: EdgeInsets.all(_alertOptions.padding),
+                    child: IconButton.outlined(
+                      icon: FaIcon(
+                        FontAwesomeIcons.xmark,
+                        size: _alertOptions.iconSize,
+                      ),
+                      onPressed: _dismissPressed,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Tooltip(
+                  message: "Delete Selected Stamp",
+                  waitDuration: AppState.toolTipDuration,
+                  child: Padding(
+                    padding: EdgeInsets.all(_alertOptions.padding),
+                    child: ValueListenableBuilder<StampManagerEntryWidget?>(
+                      valueListenable: _selectedWidget,
+                      builder: (final BuildContext context, final StampManagerEntryWidget? selWidget, final Widget? child) {
+                        return IconButton.outlined(
+                          icon: FaIcon(
+                            FontAwesomeIcons.trashCan,
+                            size: _alertOptions.iconSize,
+                          ),
+                          onPressed: (selWidget != null && !selWidget.entryData.isLocked) ? _deleteStampPressed : null,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Tooltip(
+                  message: "Load Selected Stamp",
+                  waitDuration: AppState.toolTipDuration,
+                  child: Padding(
+                    padding: EdgeInsets.all(_alertOptions.padding),
+                    child: ValueListenableBuilder<StampManagerEntryWidget?>(
+                      valueListenable: _selectedWidget,
+                      builder: (final BuildContext context, final StampManagerEntryWidget? selWidget, final Widget? child) {
+                        return IconButton.outlined(
+                          icon: FaIcon(
+                            FontAwesomeIcons.check,
+                            size: _alertOptions.iconSize,
+                          ),
+                          onPressed: selWidget != null ? _loadStamp : null,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
